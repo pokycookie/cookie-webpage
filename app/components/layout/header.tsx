f8570e203645e974a007ab27bf741127e8057c9c
pokycookie <0cookieboy0@gmail.com>
2023-06-30 23:50:34 +0900

    feat: add header links

--- app/components/layout/header.tsx
+++ app/components/layout/header.tsx
@@ -1,15 +1,54 @@
 'use client'
 
 import Link from 'next/link'
-import styles from './header.module.css'
+import styles from './header.module.scss'
+import { useEffect, useState } from 'react'
+import { usePathname } from 'next/navigation'
+import { motion } from 'framer-motion'
 
 export default function Header() {
+  const [selectedIdx, setSelectedIdx] = useState<number | null>(null)
+  const [hoverIdx, setHoverIdx] = useState<number | null>(null)
+
+  const pathname = usePathname()
+
+  useEffect(() => {
+    const idx = links.findIndex((e) => new RegExp(e.regex).test(pathname))
+    setSelectedIdx(idx < 0 ? null : idx)
+  }, [pathname])
+
   return (
     <header className={styles.header}>
       <Link href="/" className={styles.title}>
         COOKIE
       </Link>
-      <div className={styles.links}></div>
+      <div className={styles.links}>
+        {links.map((e, i) => {
+          return (
+            <Link
+              href={e.href}
+              key={i}
+              onMouseEnter={() => setHoverIdx(i)}
+              onMouseLeave={() => setHoverIdx(null)}
+            >
+              {e.title}
+            </Link>
+          )
+        })}
+        <motion.div
+          className={styles.selector}
+          animate={{
+            x: 100 * (hoverIdx ?? selectedIdx ?? 0),
+            opacity: hoverIdx !== null ? 1 : selectedIdx !== null ? 1 : 0,
+          }}
+        ></motion.div>
+      </div>
     </header>
   )
 }
+
+const links = [
+  { title: 'About Me', href: '/aboutMe', regex: /^\/aboutMe/ },
+  { title: 'Skills', href: '/skills', regex: /^\/skills/ },
+  { title: 'Portfolio', href: '/portfolio', regex: /^\/portfolio/ },
+]

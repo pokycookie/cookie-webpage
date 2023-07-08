1e922addf1ae4111600c9e9dcdfa902ae2b24622
pokycookie <0cookieboy0@gmail.com>
2023-07-08 22:13:08 +0900

    feat: add maple-meister details

---
+++ app/components/ui/text/hiddenSection.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import styles from './hiddenSection.module.scss'
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
+import { useState } from 'react'
+import { motion } from 'framer-motion'
+
+interface IProps {
+  title: string
+  deafultOpen?: boolean
+  children?: React.ReactNode
+}
+
+export default function HiddenSection(props: IProps) {
+  const [isOpen, setIsOpen] = useState(props.deafultOpen ?? false)
+
+  return (
+    <div className={styles.hiddenSection}>
+      <div
+        className={styles.titleArea}
+        onClick={() => setIsOpen((prev) => !prev)}
+      >
+        <h3>{props.title}</h3>
+        <motion.div
+          className={styles.icon}
+          animate={{ transform: isOpen ? 'rotate(180deg)' : undefined }}
+        >
+          <FontAwesomeIcon icon={faChevronDown} />
+        </motion.div>
+      </div>
+      <motion.div
+        animate={{ height: isOpen ? 'auto' : '0px' }}
+        className={styles.content}
+      >
+        {props.children}
+      </motion.div>
+    </div>
+  )
+}
--- next.config.js
+++ next.config.js
@@ -10,6 +10,9 @@ const nextConfig = {
     }
     return config
   },
+  images: {
+    domains: ['github.com'],
+  },
 }
 
 module.exports = nextConfig

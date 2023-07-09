e9557dbc048f84473d31604fbb05ee050c541c4b
pokycookie <0cookieboy0@gmail.com>
2023-07-10 01:49:19 +0900

    feat: add contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,3 +1,24 @@
+import Marquee from '@/components/ui/marquee/marquee'
+import styles from './page.module.scss'
+
 export default function Contact() {
-  return <main></main>
+  return (
+    <main className={styles.main}>
+      <section className={styles.contact}>
+        <h1>Contact</h1>
+        <p>어떤 내용이든 편하게 메일을 보내주세요! 항상 확인하고 있습니다.</p>
+        <a href="mailto:0cookieboy0@gmail.com" className={styles.email}>
+          0cookieboy0@gmail.com
+        </a>
+        <a href="mailto:0cookieboy0@gmail.com" className={styles.link}>
+          메일 보내기
+        </a>
+      </section>
+      <div className={styles.marqueeArea}>
+        <Marquee>COOKIE</Marquee>
+        <Marquee delay={3}>COOKIE</Marquee>
+        <Marquee delay={6}>COOKIE</Marquee>
+      </div>
+    </main>
+  )
 }

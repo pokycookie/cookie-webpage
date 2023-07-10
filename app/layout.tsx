86c10af44a7a22245fe33a06bf66b5b30cba7cba
pokycookie <0cookieboy0@gmail.com>
2023-07-10 16:39:47 +0900

    fix: web font

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,13 @@ interface ILayout {
 export default function RootLayout({ children }: ILayout) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="stylesheet"
+          as="style"
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.8/dist/web/static/pretendard.css"
+        />
+      </head>
       <body className={inter.className}>
         <Header />
         {children}
